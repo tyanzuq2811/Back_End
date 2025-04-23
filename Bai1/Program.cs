a33b44830d3efc31810d415dd32373dd85bafad6
tyanzuq2811 <161927908+tyanzuq2811@users.noreply.github.com>
2025-04-23 14:53:13 +0700

    Add files via upload

---
+++ Bai1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            QLCB qlcb = new QLCB();
+            qlcb.ChayChuongTrinh();
+        }
+    }
+}
